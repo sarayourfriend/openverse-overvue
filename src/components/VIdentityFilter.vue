772fa2b8832e333203383af82bc81fcd1a904866
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2022-02-26 09:23:58 -0500

    Add review requested filter

---
+++ src/components/VIdentityFilter.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { computed } from "vue"
+import { filterList, toggleFilter } from "../composables/filters"
+import { identity } from "../composables/auth"
+import { identityFilter } from "../utils/filters"
+
+const isIdentityFiltered = computed(() =>
+	filterList.value.includes(identityFilter),
+)
+
+const hasIdentity = computed(() => identity.value !== null)
+
+const toggleIdentityFilter = () => toggleFilter(identityFilter)
+</script>
+
+<template>
+	<button
+		v-if="hasIdentity"
+		@click.prevent="toggleIdentityFilter"
+		type="button"
+	>
+		{{ isIdentityFiltered ? "Show all activity" : "Show requested reviews" }}
+	</button>
+</template>
--- src/composables/auth.ts
+++ src/composables/auth.ts
@@ -1,18 +1,16 @@
-import { Ref, UnwrapRef, isRef } from "vue"
+import { isRef, watch, readonly, ref } from "vue"
 import { didHitRateLimit } from "./rate-limit"
 import { useStorage, StorageSerializers } from "./use-storage"
+import { request } from "../utils/request"
 
 const key = "overvue:gh-auth"
 export const InvalidAuth = "invalid-auth"
-export const auth = useStorage(
-	key,
-	null as string | typeof InvalidAuth | null,
-	{ serializer: StorageSerializers.string },
-)
-type Auth = typeof auth
-export const hasAuth = (
-	auth: Auth | UnwrapRef<Auth>,
-): auth is Ref<string> | string => {
+export type Auth = string | typeof InvalidAuth | null
+export const auth = useStorage(key, null as Auth, {
+	serializer: StorageSerializers.string,
+})
+
+export const hasAuth = (auth: Auth): auth is string => {
 	if (didHitRateLimit.value) return false
 
 	const value = isRef(auth) ? auth.value : auth
@@ -21,4 +19,30 @@ export const hasAuth = (
 }
 
 export const getHeaders = () =>
-	hasAuth(auth) ? { authorization: `token ${auth.value}` } : undefined
+	hasAuth(auth.value) ? { authorization: `token ${auth.value}` } : undefined
+
+const _identity = ref(null as string | null)
+export const identity = readonly(_identity)
+
+let isFetching = false
+watch(
+	auth,
+	async (auth) => {
+		if (isFetching) return
+		isFetching = true
+		try {
+			if (!hasAuth(auth)) {
+				_identity.value = null
+				return
+			}
+
+			const res = await request("GET /user", { headers: getHeaders() })
+			_identity.value = res.data.login
+		} catch (e) {
+			console.error("Unable to retrieve identity with current token", e)
+		} finally {
+			isFetching = false
+		}
+	},
+	{ immediate: true },
+)
